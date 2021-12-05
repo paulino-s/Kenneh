99c669ae24470033b95419ff202e90feef782120
paulino-s <paulinovasquez900@gmail.com>
2021-12-04 20:18:07 -0600

    block() create

--- src/blockchain/block.test.js
+++ src/blockchain/block.test.js
@@ -9,8 +9,8 @@ describe("Block", () => {
   beforeEach(() => {
     timestamp = new Date(2000, 0, 1);
     previousBlock = Block.genesis;
-    data = 't3st-d4t4';
-    hash = 'h4s4';
+    data = "t3st-d4t4";
+    hash = "h4s4";
   });
 
   it("create an instance with parameters", () => {
@@ -22,7 +22,7 @@ describe("Block", () => {
     expect(block.hash).toEqual(hash);
   });
 
-  it('use static mine()', () =>{
+  it("use static mine()", () => {
     const block = Block.mine(previousBlock, data);
 
     expect(block.hash.length).toEqual(64);
@@ -30,15 +30,17 @@ describe("Block", () => {
     expect(data).toEqual(data);
   });
 
-  it('use static hash()', () =>{
+  it("use static hash()", () => {
     hash = Block.hash(timestamp, previousBlock.hash, data);
-    const hashOutput = '9a5e2d226eef0b03107bfa9b985bad38e2bf258df6482fcc5ad7b3e88af9aafe';
+    const hashOutput =
+      "9a5e2d226eef0b03107bfa9b985bad38e2bf258df6482fcc5ad7b3e88af9aafe";
+
     expect(hash).toEqual(hashOutput);
   });
 
-  it('use static toString', () =>{
+  it("use static toString", () => {
     const block = Block.mine(previousBlock, data);
 
-    expect(typeof block.toString()).toEqual('string');
+    expect(typeof block.toString()).toEqual("string");
   });
 });
